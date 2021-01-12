1af1ca3e17e3a835b3eb896ec182939832558b4c
apalmer778 <apalmer5@xula.edu>
2021-01-12 17:10:31 -0600

    Add files via upload

--- moves.py
+++ moves.py
@@ -1,92 +1,92 @@
 # Hard coded moves to perform on the cube.
-# location is passed in and returned with the stickers in the order they should be after the turn.
+# posation is passed in and returned with the stickers in the order they should be after the turn.
 
-# Front Quarter Turn Clockwise
-def F(loc):
-	return (loc[6] + loc[14] + loc[2] +
-			loc[3] + loc[5] + loc[13] +
-			loc[19] + loc[7] + loc[8] +
-			loc[9] + loc[10] + loc[1] +
-			loc[4] + loc[12] + loc[18] +
-			loc[15] + loc[16] + loc[0] +
-			loc[11] + loc[17] + loc[20])
+# Front Quarter Turn Cposkwise
+def F(pos):
+	return (pos[6] + pos[14] + pos[2] +
+			pos[3] + pos[5] + pos[13] +
+			pos[19] + pos[7] + pos[8] +
+			pos[9] + pos[10] + pos[1] +
+			pos[4] + pos[12] + pos[18] +
+			pos[15] + pos[16] + pos[0] +
+			pos[11] + pos[17] + pos[20])
 
-# Front Quarter Turn Counterclockwise
-def Fp(loc):
-	return (loc[17] + loc[11] + loc[2] +
-			loc[3] + loc[12] + loc[4] +
-			loc[0] + loc[7] + loc[8] +
-			loc[9] + loc[10] + loc[18] +
-			loc[13] + loc[5] + loc[1] +
-			loc[15] + loc[16] + loc[19] +
-			loc[14] + loc[6] + loc[20])
+# Front Quarter Turn Countercposkwise
+def Fp(pos):
+	return (pos[17] + pos[11] + pos[2] +
+			pos[3] + pos[12] + pos[4] +
+			pos[0] + pos[7] + pos[8] +
+			pos[9] + pos[10] + pos[18] +
+			pos[13] + pos[5] + pos[1] +
+			pos[15] + pos[16] + pos[19] +
+			pos[14] + pos[6] + pos[20])
 
 # Front Half Turn
-def F2(loc):
-	return (loc[19] + loc[18] + loc[2] +
-			loc[3] + loc[13] + loc[12] +
-			loc[17] + loc[7] + loc[8] +
-			loc[9] + loc[10] + loc[14] +
-			loc[5] + loc[4] + loc[11] +
-			loc[15] + loc[16] + loc[6] +
-			loc[1] + loc[0] + loc[20])
+def F2(pos):
+	return (pos[19] + pos[18] + pos[2] +
+			pos[3] + pos[13] + pos[12] +
+			pos[17] + pos[7] + pos[8] +
+			pos[9] + pos[10] + pos[14] +
+			pos[5] + pos[4] + pos[11] +
+			pos[15] + pos[16] + pos[6] +
+			pos[1] + pos[0] + pos[20])
 
-# Right Quarter Turn Clockwise
-def R(loc):
-	return (loc[12] + loc[1] + loc[2] +
-			loc[4] + loc[18] + loc[5] +
-			loc[6] + loc[7] + loc[8] +
-			loc[0] + loc[11] + loc[17] +
-			loc[20] + loc[13] + loc[14] +
-			loc[3] + loc[10] + loc[16] +
-			loc[15] + loc[19] + loc[9])
+# Right Quarter Turn Cposkwise
+def R(pos):
+	return (pos[12] + pos[1] + pos[2] +
+			pos[4] + pos[18] + pos[5] +
+			pos[6] + pos[7] + pos[8] +
+			pos[0] + pos[11] + pos[17] +
+			pos[20] + pos[13] + pos[14] +
+			pos[3] + pos[10] + pos[16] +
+			pos[15] + pos[19] + pos[9])
 
-# Right Quarter Turn Counterclockwise
-def Rp(loc):
-	return (loc[9] + loc[1] + loc[2] +
-			loc[15] + loc[3] + loc[5] +
-			loc[6] + loc[7] + loc[8] +
-			loc[20] + loc[16] + loc[10] +
-			loc[0] + loc[13] + loc[14] +
-			loc[18] + loc[17] + loc[11] +
-			loc[4] + loc[19] + loc[12])
+# Right Quarter Turn Countercposkwise
+def Rp(pos):
+	return (pos[9] + pos[1] + pos[2] +
+			pos[15] + pos[3] + pos[5] +
+			pos[6] + pos[7] + pos[8] +
+			pos[20] + pos[16] + pos[10] +
+			pos[0] + pos[13] + pos[14] +
+			pos[18] + pos[17] + pos[11] +
+			pos[4] + pos[19] + pos[12])
 
 # Right Half Turn
-def R2(loc):
-	return (loc[20] + loc[1] + loc[2] +
-			loc[18] + loc[15] + loc[5] +
-			loc[6] + loc[7] + loc[8] +
-			loc[12] + loc[17] + loc[16] +
-			loc[9] + loc[13] + loc[14] +
-			loc[4] + loc[11] + loc[10] +
-			loc[3] + loc[19] + loc[0])
+def R2(pos):
+	return (pos[20] + pos[1] + pos[2] +
+			pos[18] + pos[15] + pos[5] +
+			pos[6] + pos[7] + pos[8] +
+			pos[12] + pos[17] + pos[16] +
+			pos[9] + pos[13] + pos[14] +
+			pos[4] + pos[11] + pos[10] +
+			pos[3] + pos[19] + pos[0])
 
-# Up Quarter Turn Clockwise
-def U(loc):
-	return (loc[3] + loc[0] + loc[1] +
-			loc[2] + loc[10] + loc[11] +
-			loc[4] + loc[5] + loc[6] +
-			loc[7] + loc[8] + loc[9] +
-			loc[12] + loc[13] + loc[14] +
-			loc[15] + loc[16] + loc[17] +
-			loc[18] + loc[19] + loc[20])
+# Up Quarter Turn Cposkwise
+def U(pos):
+	return (pos[3] + pos[0] + pos[1] +
+			pos[2] + pos[10] + pos[11] +
+			pos[4] + pos[5] + pos[6] +
+			pos[7] + pos[8] + pos[9] +
+			pos[12] + pos[13] + pos[14] +
+			pos[15] + pos[16] + pos[17] +
+			pos[18] + pos[19] + pos[20])
 
-# Up Quarter Turn Counterclockwise
-def Up(loc):
-	return (loc[1] + loc[2] + loc[3] +
-			loc[0] + loc[6] + loc[7] +
-			loc[8] + loc[9] + loc[10] +
-			loc[11] + loc[4] + loc[5] +
-			loc[12] + loc[13] + loc[14] +
-			loc[15] + loc[16] + loc[17] +
-			loc[18] + loc[19] + loc[20])
+# Up Quarter Turn Countercposkwise
+def Up(pos):
+	return (pos[1] + pos[2] + pos[3] +
+			pos[0] + pos[6] + pos[7] +
+			pos[8] + pos[9] + pos[10] +
+			pos[11] + pos[4] + pos[5] +
+			pos[12] + pos[13] + pos[14] +
+			pos[15] + pos[16] + pos[17] +
+			pos[18] + pos[19] + pos[20])
 
 # Up Half Turn
-def U2(loc):
-	return (loc[2] + loc[3] + loc[0] +
-			loc[1] + loc[8] + loc[9] +
-			loc[10] + loc[11] + loc[4] +
-			loc[5] + loc[6] + loc[7] +
-			loc[12] + loc[13] + loc[14] +
-			loc[15] + loc[16] + loc[17] +
-			loc[18] + loc[19] + loc[20])
+def U2(pos):
+	return (pos[2] + pos[3] + pos[0] +
+			pos[1] + pos[8] + pos[9] +
+			pos[10] + pos[11] + pos[4] +
+			pos[5] + pos[6] + pos[7] +
+			pos[12] + pos[13] + pos[14] +
+			pos[15] + pos[16] + pos[17] +
+			pos[18] + pos[19] + pos[20])
